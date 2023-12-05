1cace19076d1fecaf7f17bfde1852841485f4850
ILIES SKALLI <iliesskalli2@gmail.com>
2023-12-05 11:04:11 +0100

    Intégration image de profil

--- app/pages/AccountScreen.tsx
+++ app/pages/AccountScreen.tsx
@@ -1,18 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { Camera } from 'expo-camera';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, where, query } from '@firebase/firestore';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { firebaseConfig } from '../config/config';
 
-const PostScreen = () => {
-  const [image, setImage] = useState('');
-  const [hasPermission, setHasPermission] = useState(null);
+interface UserData {
+  nom: string;
+  prenom: string;
+  email: string; 
+  adresse: string;
+}
+
+export default function AccountScreen() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [image, setImage] = useState<string | null>(null);
 
   useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const auth = getAuth();
+
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const usersRef = collection(db, 'Utilisateurs');
+        const userQuery = query(usersRef, where('email', '==', user.email));
+
+        getDocs(userQuery)
+          .then((querySnapshot) => {
+            const data: UserData | null = querySnapshot.empty
+              ? null
+              : (querySnapshot.docs[0].data() as UserData);
+            setUserData(data);
+          })
+          .catch((error) => {
+            console.error('Error fetching data from Firebase:', error);
+          });
+      } else {
+        setUserData(null);
+      }
+    });
+
+    return () => unsubscribe();
   }, []);
+
+  const handleSignOut = async () => {
+    const auth = getAuth();
+
+    try {
+      await signOut(auth);
+
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
 
   const handleChooseImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -28,45 +70,34 @@ const PostScreen = () => {
     }
   };
 
-  const handleTakePhoto = async () => {
-    if (hasPermission) {
-      const result = await ImagePicker.launchCameraAsync({
-        allowsEditing: true,
-        quality: 1,
-      });
-
-      if (!result.cancelled) {
-        const takenImage = result.uri;
-        setImage(takenImage);
-      }
-    } else {
-      alert("Permission to use camera denied");
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        <TouchableOpacity onPress={handleChooseImage} style={styles.profileImageContainer}>
-          {image ? (
-            <Image source={{ uri: image }} style={styles.profileImage} />
-          ) : (
-            <Text style={styles.profileImagePlaceholder}>Select Image</Text>
-          )}
-        </TouchableOpacity>
-        <Text style={styles.userName}>John Doe</Text>
-        <Text style={styles.userInfo}>Email: john.doe@example.com</Text>
-        <Text style={styles.userInfo}>Address: 123 Main Street</Text>
-      </View>
+      <Text style={styles.title}>User Information</Text>
+      {userData && (
+        <View style={styles.profileContainer}>
+          <TouchableOpacity onPress={handleChooseImage} style={styles.profileImageContainer}>
+            {image ? (
+              <Image source={{ uri: image }} style={styles.profileImage} />
+            ) : (
+              <Text style={styles.profileImagePlaceholder}>Select Image</Text>
+            )}
+          </TouchableOpacity>
+          <Text style={styles.userInfo}>{`Nom: ${userData.nom}`}</Text>
+          <Text style={styles.userInfo}>{`Prénom: ${userData.prenom}`}</Text>
+          <Text style={styles.userInfo}>{`Email: ${userData.email}`}</Text>
+          <Text style={styles.userInfo}>{`Adresse: ${userData.adresse}`}</Text>
+        </View>
+      )}
+      <Button title="Sign Out" onPress={handleSignOut} />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    paddingTop: 30,
     backgroundColor: '#fff',
   },
   profileContainer: {
@@ -74,7 +105,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   profileImageContainer: {
-    borderRadius: 100, // pour obtenir une forme de cercle
+    borderRadius: 100,
     overflow: 'hidden',
     borderColor: 'black',
     borderWidth: 2,
@@ -92,16 +123,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: 'gray',
   },
-  userName: {
-    fontSize: 18,
+  title: {
+    fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 10,
+    marginBottom: 20,
   },
   userInfo: {
     fontSize: 16,
     marginTop: 5,
   },
 });
-
-export default PostScreen;
  
