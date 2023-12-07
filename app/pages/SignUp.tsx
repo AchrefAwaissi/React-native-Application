66042b6c57f2fbe0676912a0fe67061a9f273bab
Jean-Kelly Seguin <jean-kelly.seguin@outlook.fr>
2023-12-07 14:29:49 +0100

    Modification de la page Sign Up

--- app/pages/SignUp.tsx
+++ app/pages/SignUp.tsx
@@ -8,98 +8,103 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from '@firebase/firestore';
 import { firebaseConfig } from '../config/config';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
- 
+
 interface User {
-  name: string;
+  id: string;
+  prenom: string;
+  nom: string;
   email: string;
   password: string;
   address: string;
 }
- 
+
 const SignUpSchema = Yup.object().shape({
-  name: Yup.string().required('Required'),
+  nom: Yup.string().required('Required'),
+  prenom: Yup.string().required('Required'),
   email: Yup.string().email('Invalid email address').required('Required'),
   password: Yup.string()
-  .required('Required')
-  .min(8, 'Password is too short - should be 8 chars minimum.')
-  .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
-  .matches(/[A-Z]/, 'Password must contain at least one uppercase letter.')
-  .matches(/[a-z]/, 'Password must contain at least one lowercase letter.')
-  .matches(/[0-9]/, 'Password must contain at least one number.')
-  .matches(/[@$!%*#?&]/, 'Password must contain at least one special character.'),
+    .required('Required')
+    .min(8, 'Password is too short - should be 8 chars minimum.')
+    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter.')
+    .matches(/[0-9]/, 'Password must contain at least one number.')
+    .matches(/[@$!%*#?&]/, 'Password must contain at least one special character.'),
   address: Yup.string().required('Required'),
 });
- 
+
 const SignUp = () => {
   const navigation = useNavigation();
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   const auth = getAuth(app);
- 
-  // La logique de gestion des utilisateurs et de navigation
+
   const handleSignUp = (values: User, { setSubmitting }: FormikHelpers<User>) => {
-    // Votre logique de traitement de l'inscription
     setSubmitting(false);
-    // create a user with email and password using Firebase
+
     createUserWithEmailAndPassword(auth, values.email, values.password)
       .then((userCredential) => {
-        // user created successfully
         const user = userCredential.user;
         console.log('User signed up: ', user);
-        // show an alert with the user object
         Alert.alert('Success', 'Utilisateur créé avec succès');
-        // navigate to the Sign In page
         navigation.navigate('SignIn');
+        
+        addDoc(collection(db, 'Utilisateurs'), {
+          id: user.uid,
+          email: values.email,
+          password: values.password,
+          adresse: values.address,
+          nom: values.nom,
+          prenom: values.prenom,
+        })
+          .then((docRef) => {
+            const docId = docRef.id;
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log('Error adding doc: ', errorCode, errorMessage);
+            Alert.alert('Error', errorMessage);
+          });
       })
       .catch((error) => {
-        // user creation failed
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log('Error signing up: ', errorCode, errorMessage);
-        // show an alert with the error message
         Alert.alert('Error', errorMessage);
       });
 
-      addDoc(collection(db, 'Utilisateurs'), {
-        email: values.email,
-        password: values.password,
-        adresse: values.address,
-        name: values.name,
-      })
-        .then((docRef) => {
-          // Document added successfully
-          const docId = docRef.id;
-          // ...
-        })
-        .catch((error) => {
-          // user creation failed
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('Error adding doc: ', errorCode, errorMessage);
-        // show an alert with the error message
-        Alert.alert('Error', errorMessage);
-        });
-     
+   
   };
- 
+
   return (
     <View style={styles.container}>
       <Text style={styles.subtitle}>Sign Up</Text>
       <Formik
-        initialValues={{ name: '', email: '', password: '', address: '' }}
+        initialValues={{ nom: '', prenom: '', email: '', password: '', address: '' }}
         validationSchema={SignUpSchema}
         onSubmit={handleSignUp}
       >
         {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isSubmitting }) => (
           <>
             <TextInput
-              placeholder="Name"
-              value={values.name}
-              onChangeText={handleChange('name')}
-              onBlur={handleBlur('name')}
+              placeholder="Nom"
+              value={values.nom}
+              onChangeText={handleChange('nom')}
+              onBlur={handleBlur('nom')}
               style={styles.input}
             />
-            {errors.name && touched.name && <Text style={styles.error}>{errors.name}</Text>}
+            {errors.nom && touched.nom && <Text style={styles.error}>{errors.nom}</Text>}
+
+            <TextInput
+              placeholder="Prénom"
+              value={values.prenom}
+              onChangeText={handleChange('prenom')}
+              onBlur={handleBlur('prenom')}
+              style={styles.input}
+            />
+            {errors.prenom && touched.prenom && <Text style={styles.error}>{errors.prenom}</Text>}
+
             <TextInput
               placeholder="Email"
               value={values.email}
@@ -111,6 +116,7 @@ const SignUp = () => {
               autoCapitalize="none"
             />
             {errors.email && touched.email && <Text style={styles.error}>{errors.email}</Text>}
+
             <TextInput
               placeholder="Password"
               secureTextEntry
@@ -120,6 +126,7 @@ const SignUp = () => {
               style={styles.input}
             />
             {errors.password && touched.password && <Text style={styles.error}>{errors.password}</Text>}
+
             <TextInput
               placeholder="Address"
               value={values.address}
@@ -128,6 +135,7 @@ const SignUp = () => {
               style={styles.input}
             />
             {errors.address && touched.address && <Text style={styles.error}>{errors.address}</Text>}
+
             <View style={styles.buttonContainer}>
               <AppButton title="Sign Up" onPress={handleSubmit} disabled={isSubmitting} />
             </View>
@@ -137,7 +145,7 @@ const SignUp = () => {
     </View>
   );
 };
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -176,6 +184,5 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 });
- 
+
 export default SignUp;
- 
